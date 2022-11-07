e769df57b53cf448465b612ae390c9b801b92285
kimsubo <kimsubo@gmail.com>
2022-11-07 16:44:33 +0900

    make side bar

--- index1.js
+++ index1.js
@@ -1,6 +1,7 @@
-var container, panorama, viewer;
+var container, panorama, viewer, title;
 
 container = document.querySelector('#container');
+title = document.getElementsByClassName('title');
 panorama = new PANOLENS.ImagePanorama('./asset/pano_vr3.jpg');
 
 viewer = new PANOLENS.Viewer({container: container});
