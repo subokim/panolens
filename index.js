0c3730b65ff8e114c94cac31e1da3b5142902ffe
kimsubo <kimsubo@gmail.com>
2022-11-07 23:27:49 +0900

    commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let sidebar, cardlist, selected;
 
 cardlist = document.getElementsByClassName("card");
+sidebar = document.getElementById("panel");
 selected = 0;
 // sidebar.style.display ="none"; 
 
 function toggleSideBar() {
-    sidebar = document.getElementById("panel");
     if(sidebar.style.display == 'none') {
         sidebar.style.display = 'inline';
     } else {
@@ -14,7 +14,6 @@ function toggleSideBar() {
 }
 
 function selectCard(id) {
-    sidebar = document.getElementById("panel");
     if ( selected !== null ) cardlist[selected].classList.remove( 'selected' );
     selected = id;
     cardlist[id].classList.add( 'selected' );
